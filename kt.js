61803fc3f1707f659affb30f060a3fd64ef454ea
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2020-05-11 11:46:04 +0530

    test

--- kt.js
+++ kt.js
@@ -1,5 +1,18 @@
-exports.handler = function (request, response) {
+let AWS = require('aws-sdk');
+const translate = new AWS.Translate();
 
+exports.handler = function (request, response) {
+    translate.translateText({
+        SourceLanguageCode: "auto",
+        TargetLanguageCode: "en",
+        Text: "Hola"
+    }).promise()
+        .then(data => {
+            // your code goes here
+        })
+        .catch(err => {
+            // error handling goes here
+        });
 
     response.send({ "message": "Successfully executed" });
 }
